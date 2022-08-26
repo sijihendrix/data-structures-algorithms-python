1787058ddec2677c5700cedaee1944a4142a58e8
Siji <oluwasijibomi.bamgboye@vfdtech.ng>
2022-08-26 15:43:26 +0100

    marvel chars

---
+++ data_structures/2_Arrays/marvel.py
@@ -0,0 +1,26 @@
+heros=['spider man','thor','hulk','iron man','captain america']
+
+print(len(heros))
+
+heros.append("black panther")
+
+print(heros)
+
+heros.remove("black panther")
+print(heros)
+
+heros[3] = "black panther"
+
+print(heros)
+
+heros.remove("thor")
+heros.remove("hulk")
+
+print(heros)
+
+heros.append("doctor strange")
+
+print(heros)
+heros.sort()
+
+print(heros)
